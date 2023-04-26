2aa8a6dfd9adf4598a6317058dcdd4fe5ca3b75e
CARR0T02 <mr.gabriel.chang@gmail.com>
2023-04-26 22:06:41 +0800

    change main js location

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/script.js',
+  entry: './src/js/script.js',
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
