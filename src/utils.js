981f4503e9ccb279e50ed1c3e2ce67bf19f0d958
CARR0T02 <mr.gabriel.chang@gmail.com>
2023-04-26 09:20:20 +0800

    Add function convertToDay

---
+++ src/utils.js
@@ -0,0 +1,19 @@
+export function convertToDay(number) {
+  switch (number) {
+    case 0:
+      return 'Sunday';
+    case 1:
+      return 'Monday';
+    case 2:
+      return 'Tuesday';
+    case 3:
+      return 'Wednesday';
+    case 4:
+      return 'Thursday';
+    case 5:
+      return 'Friday';
+    case 6:
+      return 'Saturday';
+  }
+  throw new Error('Day index is out of range (0-6)');
+}
