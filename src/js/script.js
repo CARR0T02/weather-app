97027eca4a5c27ff4d6c9fea525cff54d9b49795
CARR0T02 <mr.gabriel.chang@gmail.com>
2023-04-27 13:47:12 +0800

    Implement initialise page

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,40 @@
-import './style.css';
+import '../style.css';
+import { handleSearch } from './utils.js';
+import { fetchData, fetchCurrentData, getToday, getWeekly } from './data.js';
+import { loadWeather } from './DOMcontrol.js';
+
+export async function getWeather(input = null) {
+  let weatherData;
+  if (input === null) {
+    console.log('Using IP');
+    weatherData = await fetchCurrentData();
+  } else {
+    weatherData = await fetchData(input);
+    console.log(`Searching for ${input}`);
+  }
+  const weatherToday = getToday(weatherData, '°C');
+  const weatherWeek = getWeekly(weatherData, '°C');
+  loadWeather(weatherToday, weatherWeek, null);
+  console.log(weatherData);
+  console.log(weatherToday);
+  console.table(weatherWeek);
+}
+
+function initialise() {
+  window.addEventListener('load', () => {
+    document.querySelector('body').style.visibility = 'visible';
+  });
+  const search = document.querySelector('#search');
+  search.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+      handleSearch();
+    }
+  });
+  const searchBtn = document.querySelector('#search-icon');
+  searchBtn.addEventListener('click', handleSearch);
+}
+
+initialise();
+getWeather();
+
+function loadWeekly() {}
